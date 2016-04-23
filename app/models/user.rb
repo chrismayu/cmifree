222f33286985d466e0b09a22edf43fc84168ff17
chrismayu <chrismayu@gmail.com>
2016-04-23 18:10:52 -0400

    added details subdomains etc

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,43 @@
 class User < ActiveRecord::Base
   enum role: [:user, :vip, :admin]
+  has_one :detail   
+ 
+  after_create :set_detail, :create_tenant
   after_initialize :set_default_role, :if => :new_record?
 
-  def set_default_role
-    self.role ||= :user
-  end
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+ 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+
+   validates_presence_of :subdomain
+   validates_format_of :subdomain, :with => /\A[A-Za-z0-9-]+\z/, :message => 'The subdomain can only contain alphanumeric characters and dashes.', :allow_blank => true
+   validates_uniqueness_of :subdomain, :case_sensitive => false, :message => "  is already in use"
+   validates_exclusion_of :subdomain, :in => %w( tester support blog www billing help api ), :message => "The subdomain <strong>{{value}}</strong> is reserved and unavailable."
+
+   before_validation :downcase_subdomain
+  
+
+   def set_default_role
+     self.role ||= :user
+   end
+
+ 
+
+    private
+
+    def set_detail
+      build_detail unless detail.present?
+    end   
+
+
+    def create_tenant
+      #  Apartment::Tenant.create(subdomain)
+    end    
+
+      protected
+
+    def downcase_subdomain
+    #  self.subdomain.downcase! if attribute_present?("subdomain")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,25 @@
+class SubdomainConstraint
+  def self.matches?(request)
+    subdomains = %w{ www admin help faq }
+    request.subdomain.present? && !subdomains.include?(request.subdomain)
+  end
+end
+
+
 Rails.application.routes.draw do
  
-  resources :questions
-  root to: 'visitors#index'
+  constraints SubdomainConstraint do
+     resources :questions 
+  end
+  
+  
   devise_for :users
   resources :users do
   resources :details
   end
   
+  root to: 'visitors#index'
+  
 end
+
+
